b6934223bc85971e378b50c2e8ab25cf87b50424
shuoli <shuoli@node-3.shuoli-qv36187.opennf-pg0.utah.cloudlab.us>
2018-04-26 21:04:36 -0600

    server small change

--- server.c
+++ server.c
@@ -86,7 +86,8 @@ int main(int argc, char **argv)
 
     memcpy(&event_copy, event, sizeof(*event));
     rdma_ack_cm_event(event);
-    if ( count == 10) {
+    if ( count == 2) {
+      count++;
       clock_gettime(CLOCK_MONOTONIC_RAW, &end);
       int delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
       printf("time ms: %d\n", delta_us);
